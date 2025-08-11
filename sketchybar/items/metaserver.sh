ae36f0e2ff1ff7526ef759db9fa7f0fb88b3668c
afort24 <75180732+afort24@users.noreply.github.com>
2025-08-11 15:33:13 -0700

    Metaserver Alias

---
+++ sketchybar/items/metaserver.sh
@@ -0,0 +1,8 @@
+sketchybar --add alias "MetaServer,Item-0" left \
+           --set "MetaServer,Item-0" alias.color=0x00000000 \
+                                       alias.scale=1  \
+                                       icon.padding_left=0 \
+                                       icon.padding_right=0 \
+                                       label.padding_left=0 \
+                                       label.padding_right=0 \
+                                       background.color=0xff000000 
